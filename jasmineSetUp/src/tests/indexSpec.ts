4e2d283e9a7764f328c710cf625f3c5f1c46538e
Yuguo Zhao <cheerzhao77@gmail.com>
2021-06-19 20:12:52 -0500

    Feat: add different way to test func

--- jasmineSetUp/src/tests/indexSpec.ts
+++ jasmineSetUp/src/tests/indexSpec.ts
@@ -3,38 +3,37 @@ import arrays from "../utilities/arrays";
 import numbers from "../utilities/numbers";
 import strings from "../utilities/strings";
 
+const numArr = [3, 4, 5, 6];
+const wordArr = ['cat', 'dog', 'rabbit', 'bird'];
+
 it("expect myFunc(5) to equal 25", () => {
   expect(myFunc(5)).toEqual(25);
 });
 
-it("expect addArr([1,2,3,4]) to be 10", () => {
-  expect(arrays.addArr([1, 2, 3, 4])).toBe(10);
+it ('should capitalize a string', () => {
+    expect(strings.capitalize('a sentence')).toEqual('A Sentence');
 });
 
-it("expect concatArr([3, 4, 5, 6], ['cat', 'dog', 'rabbit', 'bird']) to have size 8", () => {
-  expect(
-    arrays.concatArr([3, 4, 5, 6], ["cat", "dog", "rabbit", "bird"])
-  ).toHaveSize(8);
+it ('should be a sum greater than 10', () => {
+    expect(numbers.sum(3,10)).toBeGreaterThan(10);
 });
 
-it("expect cut3([1,2,3,4]).length to be less than or equal 3", () => {
-  expect(arrays.cut3([1, 2, 3, 4]).length).toBeLessThanOrEqual(3);
+it ('multiply 3 by 5 and be 15', () => {
+    expect(numbers.multiply(3,5)).toEqual(15);
 });
 
-it("expect sum(1.1,1) to be close to 2", () => {
-  expect(numbers.sum(1.1, 1)).toBeCloseTo(2, 0);
+it ('should add numbers in array and be truthy', () => {
+    expect(arrays.addArr(numArr)).toBeTruthy();
 });
 
-it("expect capitalize('the quick brown fox') to be 'The Quick Brown Fox'", () => {
-  expect(strings.capitalize("the quick brown fox")).toBe("The Quick Brown Fox");
+it ('should concatenate 2 arrays to not equal just 1', () => {
+    expect(arrays.concatArr(numArr, wordArr)).not.toEqual(numArr);
 });
 
-it("exepct multiple(4,0) to equal 0", () => {
-  expect(numbers.multiply(4, 0)).toEqual(0);
+it ('should not contain the third index', () => {
+    expect(arrays.cut3(wordArr)).not.toContain('rabbit');
 });
 
-it("expect lgNum([3, 4, 5, 6, 'cat', 'dog', 'rabbit', 'bird']) to be greater than or equal 6", () => {
-  expect(
-    arrays.lgNum([3, 4, 5, 6, "cat", "dog", "rabbit", "bird"])
-  ).toBeGreaterThanOrEqual(6);
+it ('should not have a large number and be falsy', () => {
+    expect(arrays.lgNum(wordArr)).toBeFalsy();
 });
